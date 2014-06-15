bfd0d8ea4059c21aa8e775b28bf18c4ff00194ba
Erik Westrup <erik.westrup@gmail.com>
2014-06-15 21:52:32 +0200

    Remove warning by not using static imports.

--- Greed/tests/src/se/umu/androidcourse/erwe0033/greed/tests/GreedGameTest.java
+++ Greed/tests/src/se/umu/androidcourse/erwe0033/greed/tests/GreedGameTest.java
@@ -3,7 +3,7 @@ package se.umu.androidcourse.erwe0033.greed.tests;
 import java.util.Set;
 
 import android.test.AndroidTestCase;
-import static junit.framework.Assert.*;
+import junit.framework.Assert;
 
 import se.umu.androidcourse.erwe0033.greed.GreedGame;
 import se.umu.androidcourse.erwe0033.greed.Die;
@@ -12,12 +12,12 @@ public class GreedGameTest extends AndroidTestCase {
 
     public void testNewGame() {
     	GreedGame game = new GreedGame();
-    	assertEquals("Total score not initially correct", 0, game.getTotalScore());
-    	assertEquals("Round score not initially correct.", 0, game.getRoundScore());
+    	Assert.assertEquals("Total score not initially correct", 0, game.getTotalScore());
+    	Assert.assertEquals("Round score not initially correct.", 0, game.getRoundScore());
     }
 
 	private void dieInitTest(Die die) {
-		assertEquals("Die initial value correct.", 0, die.getValue());
+		Assert.assertEquals("Die initial value correct.", 0, die.getValue());
 	}
 
     public void testNewDice() {
@@ -28,7 +28,7 @@ public class GreedGameTest extends AndroidTestCase {
     public void testInitialDice() {
     	GreedGame game = new GreedGame();
     	Set<Die> dice = game.getDice();
-    	assertEquals("Not correct number of dice.", 6, dice.size());
+    	Assert.assertEquals("Not correct number of dice.", 6, dice.size());
     	for (Die die : dice) {
     		dieInitTest(die);
     	}
