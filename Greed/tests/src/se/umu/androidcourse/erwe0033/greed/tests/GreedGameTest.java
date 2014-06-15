57b4ee4c23738fabbd39b909f654792dd72c0c0c
Erik Westrup <erik.westrup@gmail.com>
2014-06-15 21:07:59 +0200

    Moved test file.

---
+++ Greed/tests/src/se/umu/androidcourse/erwe0033/greed/tests/GreedGameTest.java
@@ -0,0 +1,12 @@
+package se.umu.androidcourse.erwe0033.greed.tests;
+
+
+import android.test.AndroidTestCase;
+import static junit.framework.Assert.*;
+
+public class GreedGameTest extends AndroidTestCase {
+
+    public void testSomething() {
+       assertTrue(1 + 1 == 2);
+    }
+}
