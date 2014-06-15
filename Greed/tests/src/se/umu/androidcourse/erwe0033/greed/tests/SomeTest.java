77a34a3162fa8f0f5ce86a2aa38cc867e525ec20
Erik Westrup <erik.westrup@gmail.com>
2014-06-15 19:48:54 +0200

    Simple working test case.

---
+++ Greed/tests/src/se/umu/androidcourse/erwe0033/greed/tests/SomeTest.java
@@ -0,0 +1,16 @@
+package se.umu.androidcourse.erwe0033.greed.tests;
+
+
+import android.test.AndroidTestCase;
+import static junit.framework.Assert.*;
+
+public class SomeTest extends AndroidTestCase {
+
+    public void testSomething() throws Throwable {
+       assertTrue(1 + 1 == 2);
+    }
+
+    public void testSomethingElse() throws Throwable {
+       assertFalse(1 + 1 == 3);
+    }
+}
